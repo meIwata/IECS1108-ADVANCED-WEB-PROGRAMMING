ffd20808b2f343f6cdc267699e75bbd87bd45f3d
waleilulu <waleilulu@gmail.com>
2025-07-09 12:07:21 +0800

    update

---
+++ vue-20250709/src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// import App from './components/shoppinglist.vue' // 首頁改成自己的shoppinglist
+
+createApp(App).mount('#app')
